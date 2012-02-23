fac1cec53221080eed33188165322d42ac31e1b0
Nick <nicholas.anders.clark@gmail.com>
2012-02-23 14:17:27 +0100

    Problem 1.1 completed

--- Assignment2/Code/readbodyfatNickCopy.R
+++ Assignment2/Code/readbodyfatNickCopy.R
@@ -1,7 +1,7 @@
 #
 #  Reads the body fat data set from the bodyfat.mat file.
 #
-
+require ('MASS')
 load( '/Users/Nick/Documents/Bioinformatics/Machine Learning/Machine-Learning/Assignment2/Data/bodyfat.RData' )
 
 ridx <- sample( 1:dim( data )[1], dim( data )[1] )
@@ -26,9 +26,42 @@ cols2train <- as.matix(cols2train)
 
 targettrain <- train[c("pctbodyfat")]
 targettrain <- as.matrix(targettrain)
-  
+
+targettest <- test[c("pctbodyfat")]
+targettest <- as.matrix(targettest)
+
+targettrain2 <- train[c("pctbodyfat")]
+targettrain2 <- as.matrix(targettrain2)
+
+targettest2 <- test[c("pctbodyfat")]
+targettest2 <- as.matrix(targettest2)
+
 cols1train <- as.matrix(cols1train)
 cols1trainwithone <- cbind(seq(1,1,length.out=length(cols1train)), cols1train)
 
+cols2train <- as.matrix(cols2train)
+cols2trainwithone <- cbind(seq(1,1,length.out=length(cols2train)), cols2train)
+
 ## calculate the weights with identity basis function using function (3.15)
-y <-solve(t(cols1trainwithone)%*%cols1trainwithone)%*%t(cols1trainwithone)%*%targettrain
+y <-ginv(t(cols1trainwithone)%*%cols1trainwithone)%*%t(cols1trainwithone)%*%targettrain
+
+## add column of ones to test data for first model
+cols1testwithone <- cbind(seq(1,1,length.out=length(cols1test)), cols1test)
+
+## calculate prediction values
+pred<- cols1testwithone%*%y
+
+##calculate RMS for first specification - 3.05
+RMS <- sqrt((1/length(pred)*(sum(pred-targettest))^2))
+
+## Deternime RMS for second procedure
+y2 <-ginv(t(cols2trainwithone)%*%cols2trainwithone)%*%t(cols2trainwithone)%*%targettrain2
+
+## add column of ones to test data for first model
+cols2testwithone <- cbind(seq(1,1,length.out=length(cols2test)), cols2test)
+
+## calculate prediction values
+pred2<- cols2testwithone%*%y2
+
+##calculate RMS for first specification - 2.59
+RMS2 <- sqrt((1/length(pred2)*(sum(pred2-targettest2))^2))
